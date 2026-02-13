d6143ca0154d8c085b2b26c76e607249a2b4e3c9
oleksandr_smiian <oleksandr_smiian@tchdm.live>
2026-02-13 16:36:00 +0200

    fix build v3

--- azamaza/src/app/page.tsx
+++ azamaza/src/app/page.tsx
@@ -1,28 +1,20 @@
-import meta from './meta';
 import Page from "@/app/_modules/Page";
 import Home from "@/app/_modules/Home";
 import './_assets/global.css';
+//
+// export async function generateMetadata({
+//                                            params,
+//                                        }: {
+//     params: { lang: string };
+// }) {
+//     const { lang } = params;
+//     return {
+//         title: meta[lang]?.title || meta.en.title,
+//         description: meta[lang]?.description || meta.en.description,
+//     };
+// }
 
-export async function generateMetadata({
-                                           params,
-                                       }: {
-    params: { lang: string };
-}) {
-    const { lang } = params;
-    return {
-        title: meta[lang]?.title || meta.en.title,
-        description: meta[lang]?.description || meta.en.description,
-    };
-}
-
-export default async function HomePage({
-                                           params,
-                                           searchParams,
-                                       }: {
-    params: { lang: string };
-    searchParams: {};
-}) {
-    const { lang } = params;
+export default async function HomePage() {
 
     return (
         <Page>
