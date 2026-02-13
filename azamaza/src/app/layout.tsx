77bfd39200e3be8e69ff5e975d0a981ccde3f730
oleksandr_smiian <oleksandr_smiian@tchdm.live>
2026-02-13 16:28:00 +0200

    fix build

--- azamaza/src/app/layout.tsx
+++ azamaza/src/app/layout.tsx
@@ -1,14 +1,11 @@
 export default async function LangLayout({
                                              children,
-                                             params,
                                          }: {
     children: React.ReactNode;
-    params: { lang: string };
 }) {
-    const { lang } = params;
 
     return (
-        <html lang={lang}>
+        <html lang="en">
         <head>
             <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
             <link rel="icon" type="image/png" sizes="96x96" href="/images/favicon-96x96.png" />
